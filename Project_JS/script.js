744042690f9925a8a69067cd86b2a46303eaab1e
Marlon <marlonmuchanga@gmail.com>
2026-03-25 22:50:22 +0100

    Criar  jaca_script da pagina de tarefas

---
+++ Project_JS/script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("formTarefa");
+const input = document.getElementById("inputTarefa");
+const mensagem = document.getElementById("mensagem");
+const lista = document.getElementById("listaTarefas");
+
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+
+  const texto = input.value.trim();
+
+  if (texto === "") return;
+
+  const item = document.createElement("li");
+  item.textContent = texto;
+  lista.appendChild(item);
+
+  mensagem.classList.remove("oculto");
+  setTimeout(function () {
+    mensagem.classList.add("oculto");
+  }, 2000);
+
+  input.value = "";
+  input.focus();
+});
